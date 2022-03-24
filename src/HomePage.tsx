783440c7b447451ed418aaa3f1599d178451ae97
Froylan  Vargas Gomez <froy@MacBook-Pro-de-Froylan.local>
2022-03-24 10:29:07 -0600

    init app

---
+++ src/HomePage.tsx
@@ -0,0 +1,62 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+import { QuestionList } from './QuestionList';
+import { getUnansweredQuestions } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
+import { PrimaryButton } from './Styles';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  gettingUnansweredQuestionsAction,
+  gotUnansweredQuestionsAction,
+  AppState,
+} from './Store';
+
+export const HomePage = () => {
+  const dispatch = useDispatch();
+  const questions = useSelector(
+    (state: AppState) => state.questions.unanswered,
+  );
+  const questionsLoading = useSelector(
+    (state: AppState) => state.questions.loading,
+  );
+
+  React.useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      dispatch(gettingUnansweredQuestionsAction());
+      const unansweredQuestions = await getUnansweredQuestions();
+      dispatch(gotUnansweredQuestionsAction(unansweredQuestions));
+    };
+    doGetUnansweredQuestions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const navigate = useNavigate();
+  const handleAskQuestionClick = () => {
+    navigate('ask');
+  };
+
+  return (
+    <Page>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        `}
+      >
+        <PageTitle>Unanswered Questions</PageTitle>
+        <PrimaryButton onClick={handleAskQuestionClick}>
+          Ask a question
+        </PrimaryButton>
+      </div>
+      {questionsLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <QuestionList data={questions} />
+      )}
+    </Page>
+  );
+};
---
+++ src/SearchPage.tsx
@@ -0,0 +1,50 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useSearchParams } from 'react-router-dom';
+import { QuestionList } from './QuestionList';
+import { searchQuestions } from './QuestionsData';
+
+import React from 'react';
+import { Page } from './Page';
+
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  AppState,
+  searchingQuestionsAction,
+  searchedQuestionsAction,
+} from './Store';
+
+export const SearchPage = () => {
+  const dispatch = useDispatch();
+  const questions = useSelector((state: AppState) => state.questions.searched);
+
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('criteria') || '';
+
+  React.useEffect(() => {
+    const doSearch = async (criteria: string) => {
+      dispatch(searchingQuestionsAction());
+      const foundResults = await searchQuestions(criteria);
+      dispatch(searchedQuestionsAction(foundResults));
+    };
+    doSearch(search);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]);
+
+  return (
+    <Page title="Search Results">
+      {search && (
+        <p
+          css={css`
+            font-size: 16px;
+            font-style: italic;
+            margin-top: 0px;
+          `}
+        >
+          for "{search}"
+        </p>
+      )}
+      <QuestionList data={questions} />
+    </Page>
+  );
+};
